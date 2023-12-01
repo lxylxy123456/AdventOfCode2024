bc31462a2e003224a60ba8e7a8c8b1830166a1b3
Eric Li <lixiaoyi13691419520@gmail.com>
2023-11-30 21:53:42 -0800

    2022/d03

--- practice2022/d03/s.py
+++ practice2022/d03/s.py
@@ -28,16 +28,31 @@ def main():
 	if not args.one:
 		print(part_2(lines))
 
+def score(s):
+	if 'a' <= s <= 'z':
+		return ord(s) - ord('a') + 1
+	if 'A' <= s <= 'Z':
+		return ord(s) - ord('A') + 27
+
 def part_1(lines):
 	s = 0
 	for i in lines:
-		i
+		assert len(i) % 2 == 0
+		a = set(i[:len(i)//2]).intersection(set(i[len(i)//2:]))
+		assert len(a) == 1
+		s += score(next(iter(a)))
 	return s
 
 def part_2(lines):
 	s = 0
-	for i in lines:
-		i
+	count = len(lines) // 3
+	assert len(lines) % 3 == 0
+	lines = iter(lines)
+	for i in range(count):
+		j0, j1, j2 = next(lines), next(lines), next(lines)
+		a = set(j0).intersection(j1).intersection(j2)
+		assert len(a) == 1
+		s += score(next(iter(a)))
 	return s
 
 if __name__ == '__main__':
