2f4b73c73de695ee12f638137a79688e4f1502cf
Eric Li <lixiaoyi13691419520@gmail.com>
2023-12-03 21:17:31 -0800

    q.txt

--- s.py
+++ s.py
@@ -1,5 +1,5 @@
 import argparse, math, sys, re, functools, operator, itertools
-from collections import defaultdict
+from collections import defaultdict, Counter
 #sys.setrecursionlimit(100000000)
 #A = list(map(int, input().split()))
 #T = int(input())
