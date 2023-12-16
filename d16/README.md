1adf46a1ff8b2b298c8d0cef85c18b623fa4c9cc
Eric Li <lixiaoyi13691419520@gmail.com>
2023-12-15 21:21:15 -0800

    q.txt

---
+++ d16/README.md
@@ -0,0 +1,3 @@
+Youtube Video:
+
+[![Youtube Video](http://img.youtube.com/vi/tXR-1t0AkTg/0.jpg)](http://www.youtube.com/watch?v=tXR-1t0AkTg)
--- d16/s.py
+++ d16/s.py
@@ -1,3 +1,5 @@
+# Youtube: https://youtu.be/tXR-1t0AkTg
+
 import argparse, math, sys, re, functools, operator, itertools
 from collections import defaultdict, Counter
 #sys.setrecursionlimit(100000000)
