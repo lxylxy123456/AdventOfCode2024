67fcd52a3999ce717258c123816f9a9f227363a5
Eric Li <lixiaoyi13691419520@gmail.com>
2023-12-18 21:35:46 -0800

    q.txt

--- d19/s.py
+++ d19/s.py
@@ -1,3 +1,5 @@
+# Youtube: https://youtu.be/ZakocMnsFDk
+
 import argparse, math, sys, re, functools, operator, itertools, heapq
 from collections import defaultdict, Counter
 #sys.setrecursionlimit(100000000)
