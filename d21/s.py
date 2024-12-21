91076bf5a617e8c4e7ffc606ec902a3a44002516
Eric Li <lixiaoyi13691419520@gmail.com>
2024-12-20 22:50:16 -0800

    debug.py

--- d21/s.py
+++ d21/s.py
@@ -40,7 +40,7 @@ def _get_paths(begin, end, loc):
 		ans += (ey - by) * '>'
 	else:
 		ans += (by - ey) * '<'
-	# Previous version did not handle the problem of moving to empty space
+	# Previous version did not handle the problem of moving to empty space.
 	ret = []
 	for i in set(itertools.permutations(ans)):
 		valid = True
@@ -117,71 +117,12 @@ def solve_moves1(text, levels):
 	#print(repr(text), repr(levels), repr(ans))
 	return ans
 
-def simulate1(text, level):
-	loc = {'n': LOC_NUM, 'd': LOC_DIR}[level]
-	rev_loc = dict(map(lambda x: (x[1], x[0]), loc.items()))
-	cx, cy = loc['A']
-	ans = ''
-	for i in text:
-		if i == 'A':
-			ans += rev_loc[(cx, cy)]
-		elif i == '<':
-			cy -= 1
-		elif i == 'v':
-			cx += 1
-		elif i == '>':
-			cy += 1
-		elif i == '^':
-			cx -= 1
-		else:
-			raise ValueError
-		#print(i, cx, cy)
-	assert rev_loc[(cx, cy)] == 'A'
-	return ans
-
-def simulate_levels1(text, levels):
-	cur = text
-	for level in levels:
-		cur = simulate1(cur, level)
-	return cur
-
-#print(simulate1('<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A', 'd'))
-#print(simulate1('v<<A>>^A<A>AvA<^AA>A<vAAA>^A', 'd'))
-#print(simulate1('<A^A>^^AvvvA', 'n'))
-
-_0 = '<vA<AA>^>AAvA<^A>AvA^Av<<A>^>AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A'
-_1 = 'v<<AA>^A>A<AA>AvAA^A<vAAA>^A'
-_2 = '<<^A^^A>>AvvvA'
-_3 = '179A'
-assert len(_0) == 64
-assert simulate1(_0, 'd') == _1
-assert simulate1(_1, 'd') == _2
-assert simulate1(_2, 'n') == _3
-
-r0 = '<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A'
-r1 = '<Av<AA>>^A<AA>AvAA^A<vAAA>^A'
-r2 = '^<<A^^A>>AvvvA'
-r3 = '179A'
-assert len(r0) == 68
-assert simulate1(r0, 'd') == r1
-assert simulate1(r1, 'd') == r2
-assert simulate1(r2, 'n') == r3
-
-for i in [
-	'<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A',
-	'<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A',
-	'<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A',
-	'<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A',
-	'<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A',
-]:
-	print(simulate_levels1(i, 'ddn'))
-
 def part_1(lines):
 	s = 0
 	levels = 'ndd'
 	for i in lines:
 		seq = solve_moves1(i, levels)
-		print(len(seq), int(i.strip('A')))
+		#print(len(seq), int(i.strip('A')))
 		s += len(seq) * int(i.strip('A'))
 	return s
 
